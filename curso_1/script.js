4292e550f2696dd847ad9c4900571c6d18591d3c
bonnesbaby14 <bonnesbaby14@gmail.com>
2024-06-26 17:39:22 -0600

    js copy clipboard

--- curso_1/script.js
+++ curso_1/script.js
@@ -1,5 +1,5 @@
 
-var last_word="";
+var last_word = "";
 
 function encry(text = "ejemplo") {
 
@@ -9,32 +9,32 @@ function encry(text = "ejemplo") {
 
 
     for (let index = 0; index < text.length; index++) {
-        
+
 
         switch (text[index]) {
             case "e":
-                new_text+="enter"
+                new_text += "enter"
 
                 break;
             case "i":
-                new_text+="imes"
+                new_text += "imes"
 
                 break;
             case "a":
-                new_text+="ai"
+                new_text += "ai"
 
                 break;
             case "o":
-                new_text+="ober"
+                new_text += "ober"
 
                 break;
             case "u":
-                new_text+="ufat"
+                new_text += "ufat"
 
                 break;
 
             default:
-                new_text+=text[index]
+                new_text += text[index]
 
                 break;
         }
@@ -51,32 +51,32 @@ function encry(text = "ejemplo") {
     return new_text
 }
 
-function desencry(text="gaitober") {
-    text=text.toLowerCase()
+function desencry(text = "gaitober") {
+    text = text.toLowerCase()
 
-    while(text.includes("enter")){
+    while (text.includes("enter")) {
 
-        text=text.replace("enter","e")
+        text = text.replace("enter", "e")
     }
 
-    while(text.includes("imes")){
-        text=text.replace("imes","i")
-     
+    while (text.includes("imes")) {
+        text = text.replace("imes", "i")
+
     }
 
-    while(text.includes("ai")){
-        text=text.replace("ai","a")
-     
+    while (text.includes("ai")) {
+        text = text.replace("ai", "a")
+
     }
-    while(text.includes("ober")){
-        text=text.replace("ober","o")
-     
+    while (text.includes("ober")) {
+        text = text.replace("ober", "o")
+
     }
 
 
-    while(text.includes("ufat")){
-        text=text.replace("ufat","u")
-     
+    while (text.includes("ufat")) {
+        text = text.replace("ufat", "u")
+
     }
 
 
@@ -86,29 +86,38 @@ function desencry(text="gaitober") {
 }
 
 
-function encriptar(){
+function encriptar() {
 
-    var text=document.getElementById("text-prompt").value;
-    
-    document.getElementById("not-found-text").style.display="none"
+    var text = document.getElementById("text-prompt").value;
 
-    document.getElementById("found-text").style.display=""
+    document.getElementById("not-found-text").style.display = "none"
 
-    document.getElementById('result_text').innerText =encry(text);
+    document.getElementById("found-text").style.display = ""
 
-    last_word= encry(text)
+    document.getElementById('result_text').innerText = encry(text);
+
+    last_word = encry(text)
 
 }
 
 
-function desencriptar(){
-    var text=document.getElementById("text-prompt").value;
-    
-    document.getElementById("not-found-text").style.display="none"
+function desencriptar() {
+    var text = document.getElementById("text-prompt").value;
 
-    document.getElementById("found-text").style.display=""
+    document.getElementById("not-found-text").style.display = "none"
 
-    document.getElementById('result_text').innerText =desencry(text);
+    document.getElementById("found-text").style.display = ""
 
-    last_word= desencry(text)
+    document.getElementById('result_text').innerText = desencry(text);
+
+    last_word = desencry(text)
+}
+
+async function copy() {
+    navigator.clipboard.writeText(last_word).then(function () {
+
+        alert("Texto copiado")
+    }, function (err) {
+
+    });
 }
